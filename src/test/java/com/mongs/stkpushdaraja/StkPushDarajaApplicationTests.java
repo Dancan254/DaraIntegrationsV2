d0f787555b1c40ec13dc17350d137c87a761d4bb
Dancan254 <dancanian25@gmail.com>
2025-03-15 09:44:43 +0300

    Format test method for consistency in StkPushDarajaApplicationTests

--- src/test/java/com/mongs/stkpushdaraja/StkPushDarajaApplicationTests.java
+++ src/test/java/com/mongs/stkpushdaraja/StkPushDarajaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class StkPushDarajaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
