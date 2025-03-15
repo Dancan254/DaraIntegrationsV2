fa4e0d3ba8f65adfa8358ab92ab1a6b6b291cf43
Dancan254 <dancanian25@gmail.com>
2025-03-15 09:41:37 +0300

    Initialize STKPushDaraja project with Spring Boot setup and Maven wrapper

---
+++ src/test/java/com/mongs/stkpushdaraja/StkPushDarajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongs.stkpushdaraja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StkPushDarajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
