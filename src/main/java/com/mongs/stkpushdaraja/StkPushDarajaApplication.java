82ff44e7fae8bf1bff8aaf1d4aa2b3af197f5bbe
Dancan254 <dancanian25@gmail.com>
2025-03-15 09:42:51 +0300

    Format main method in StkPushDarajaApplication for consistency

--- src/main/java/com/mongs/stkpushdaraja/StkPushDarajaApplication.java
+++ src/main/java/com/mongs/stkpushdaraja/StkPushDarajaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StkPushDarajaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StkPushDarajaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StkPushDarajaApplication.class, args);
+    }
 
 }
